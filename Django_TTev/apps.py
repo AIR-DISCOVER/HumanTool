06201f3e873028cd2e88a6c9064675c1261f17e4
Bingxi Zhao <150592536+pancacake@users.noreply.github.com>
2025-09-06 00:45:16 +0800

    Delete Django_TTev directory

--- Django_TTev/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class EvaluationConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Django_TTev'  # 修复应用名称
-    verbose_name = 'TATA评估系统'
--- Django_TTev/templatetags/__init__.py
+++
@@ -1,1 +0,0 @@
-# 这个文件使得 templatetags 成为一个 Python 包
