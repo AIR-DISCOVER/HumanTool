48ee96ebf98ebcb1ec596533159a76df3214e401
yuanrong <tangxtong2022@gmail.com>
2025-09-08 08:13:01 +0800

    Remove CreativeWritingEva submodule

--- CreativeWritingEva/tata_evaluation/urls.py
+++
@@ -1,9 +0,0 @@
-"""tata_evaluation URL Configuration"""
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('management/', include('Django_TTev.admin_urls')),  # 管理后台
-    path('', include('Django_TTev.urls')),  # 使用你的应用的URLs
-]
